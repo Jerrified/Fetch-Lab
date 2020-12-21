1dc273ccb0bcf2e65ce236852fb7c48635d7cdab
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-12-21 15:47:09 -0500

    Adding Fetch API

---
+++ script.js
@@ -0,0 +1,25 @@
+// -------------------
+// Fetch Functions
+// -------------------
+fetch('https://rickandmortyapi.com/api')
+
+
+
+// -----------
+// Button
+// -----------
+
+
+
+// Create a button that
+    // How am I going to create the button?
+        // DOM Manipulation
+// when clicked, displays the data generated from the API.
+    // ?Not sure need to figure out how to click a button and make something happen?
+        // Note Trey thinks it might have to do with onclick
+// Create another button 
+    // Create it with DOM
+// that randomly shuffles the data generated from the API.
+    // ?How do I shuffle data?
+        // Possible math.random?
+// Minimal styling for the page is required utilizing the DOM.

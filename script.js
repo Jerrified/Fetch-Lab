227b95209cb8ab86e8a6680cb3ecefda159c66de
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-12-22 18:24:59 -0500

    Added button

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-// -------------------
-// Fetch Functions
-// -------------------
-fetch("https://type.fit/api/quotes")
+const url = "https://type.fit/api/quotes";
+    fetch(url)
+    .then(unconvertedQuotes => unconvertedQuotes.json())
+    .then(convertedJson => {
+        let data = convertedJson;
 
+        console.log(data)
 
-// -----------
-// Button
-// -----------
-let btn = document.createElement('button');
-btn.innerHTML = 'Click for a new Rick and Morty character!';
-document.body.appendChild(btn);
+        function newQuote() {
+            let randomQuote = Math.floor(Math.random() * (data.length));
 
+            document.getElementById('container').innerHTML = data[randomQuote];
+        }
 
-// --------------
-// Button Function
-// ---------------
-
-
-
-// Create a button that
-    // How am I going to create the button?
-        // DOM Manipulation
-// when clicked, displays the data generated from the API.
-    // ?Not sure need to figure out how to click a button and make something happen?
-        // Note Trey thinks it might have to do with onclick
-// Create another button 
-    // Create it with DOM
-// that randomly shuffles the data generated from the API.
-    // ?How do I shuffle data?
-        // Possible math.random?
-// Minimal styling for the page is required utilizing the DOM.
+        let btn = document.createElement('button');
+        btn.innerHTML = 'Click for a new quote.';
+        document.body.appendChild(btn);
+        button.addEventListner('onclick', newQuote());
+    })
